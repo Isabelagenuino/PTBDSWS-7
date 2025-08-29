852533e4e097ffe90b899c4067680658f4e2d519
Isabelagenuino <isabelagenuinodeoliveira@gmail.com>
2025-08-28 21:17:50 -0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -22,6 +22,6 @@ def user(name):
 def page_not_found(e):
     return render_template('404.html'), 404
 
-@app.errorhandler(500)
+@app.errorhandler(404)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template('404.html'), 404

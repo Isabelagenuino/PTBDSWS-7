3e2e7c74f4757b7e6a83e3bd296112e04b0e2e78
IsabelaG <isabelagenuinodeoliveira@gmail.com>
2025-08-29 00:06:12 +0000

    a

--- flask_app.py
+++ flask_app.py
@@ -1,36 +1,27 @@
 
-# A very simple Flask Hello World app for you to get started with...
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
-from flask import Flask, request, make_response, redirect, abort
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
-def hello_world():
-    return '''
-    <h1>Avaliação contínua: Aula 030</h1>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/user/Isabela Genuino/PT3033317/IFSP">Identificação</a></li>
-            <li><a href="/contextorequisicao">Contexto da requisição</a></li></ul>'''
+def home():
+    return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route('/user/<name>/<pt>/<campus>')
-def hello_user(name, pt, campus):
-    return f'''
-    <h1>Avaliação contínua: Aula 030</h1>
-        <h2>Aluno: {name}</h2>
-        <h2>Prontuário: {pt}</h2>
-        <h2>Instituição: {campus}</h2>
-        <p><a href="/">Voltar</a></p>'''
 
-@app.route('/contextorequisicao')
-def contextorequisicao():
-    navegador = request.headers.get('User-Agent')
-    ipPcRemoto = request.remote_addr
-    host = request.host
-    return f'''
-        <h1>Avaliação contínua: Aula 030</h1>
-        <h2>Seu navegador é: {navegador}</h2>
-        <h2>O IP do computador remoto é: {ipPcRemoto}</h2>
-        <h2>O host da aplicação é: {host}</h2>
-        <p><a href="/">Voltar</a></p>'''
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name = name)
+
+@app.route('/rotainexistente')
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500

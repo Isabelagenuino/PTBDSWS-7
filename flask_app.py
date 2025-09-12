d36f4310aba905a3e36ec6edee07b6e629331bfa
Isabelagenuino <isabelagenuinodeoliveira@gmail.com>
2025-09-11 21:18:30 -0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,30 @@
-from flask import Flask, render_template, url_for, flash, session
+from flask import Flask, render_template, url_for, flash, session, redirect
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
-
-from flask_
+from flask_wtf import Flaskform
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET KEY'] = 'Chave forte'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html', current_time = datetime.utcnow())
+class Nameform(FlaskForm):
+    name = StringField('What is your name?', validators = [DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    form = NameForm()
+    if form.validade_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.date:
+            flash('Looks like you have changed your name!')
+            session['name'] = form.name.data
+            return redirect(url_for('index'))
+    return render_template('index.html', form = form, name= session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):

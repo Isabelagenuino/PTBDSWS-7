2870e9dcd5f4237fceed35f4dc2e1da3d6e660d4
Isabelagenuino <isabelagenuinodeoliveira@gmail.com>
2025-09-25 21:38:42 -0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -9,6 +9,11 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, PasswordField
 from wtforms.validators import DataRequired
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -16,11 +21,31 @@ moment = Moment(app)
 
 app.config['SECRET_KEY'] = "Chave forte"
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 class NameForm(FlaskForm):
-    name = StringField("Informe o seu nome", validators = [DataRequired()])
-    sobrenome = StringField("Informe o seu sobrenome:", validators = [DataRequired()])
-    instituicao = StringField("Informe a sua Insituição de ensino:", validators = [DataRequired()])
-    disciplina = SelectField("Informe a sua disciplina:", choices = [('DSWA5', 'DSWA5'), ('DWBA4', 'DWBA4'), ('Gestão de Projetos', 'Gestão de projetos')], validators = [DataRequired()])
+    name = StringField("What is your name?", validators = [DataRequired()])
     submit = SubmitField('Submit')
 
 class LoginForm(FlaskForm):
@@ -28,21 +53,27 @@ class LoginForm(FlaskForm):
     senha = PasswordField('Informe a sua senha', validators = [DataRequired()], render_kw={"placeholder": "Informe a sua senha"})
     submit = SubmitField('Enviar')
 
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
 @app.route('/', methods=['GET','POST'])
 def index():
     form = NameForm()
-    ip = request.remote_addr
-    host = request.host
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user_role = Role.query.filter_by(name='User').first()
+            user = User(username=form.name.data, role = user_role)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
-        session['sobrenome'] = form.sobrenome.data
-        session['instituicao'] = form.instituicao.data
-        session['disciplina'] = form.disciplina.data
         return redirect(url_for('index'))
-    return render_template('index.html', form = form, nome = session.get('name'), sobrenome = session.get('sobrenome'), instituicao = session.get('instituicao'), disciplina = session.get('disciplina'), ip = ip, host = host, momento = datetime.utcnow())
+    usuarios = User.query.all()
+    return render_template('index.html', form=form, nome=session.get('name'), known=session.get('known', False), usuarios = usuarios)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -50,11 +81,12 @@ def login():
     if form.validate_on_submit():
         session['usuario'] = form.usuario.data
         return redirect(url_for('loginResponse'))
-    return render_template('login.html', form = form, momento = datetime.utcnow())
+    return render_template('login.html', form = form)
 
 @app.route('/loginResponse')
 def loginResponse():
-    return render_template('loginResponse.html', usuario = session.get('usuario'), momento = datetime.utcnow())
+    return render_template('loginResponse.html', usuario = session.get('usu
+ario'))
 
 @app.route('/user/<name>')
 def user(name):
@@ -78,23 +110,3 @@ def contextorequisicao(nome):
     IP = request.remote_addr
     host = request.host
     return render_template('contextorequisicao.html', nome=nome, requisicao=requisicao, IP=IP, host=host)
-
-@app.route('/codigostatusdiferente')
-def codigostatusdiferente():
-    codigo = request.args['codigo']
-    return f'<p>{codigo}</p>'
-
-@app.route('/objetoresposta')
-def objetoresposta():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer','42')
-    return response
-
-@app.route('/redirecionamento')
-def redirecionamento():
-    return redirect('https://ptb.ifsp.edu.br/')
-
-from flask import abort
-@app.route('/abortar')
-def abortar():
-    abort(404)
